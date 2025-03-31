45b40c8d529157337930a4cb8cc228becdfd56bd
Prossi1 <142547708+Prossi1@users.noreply.github.com>
2025-03-31 13:13:07 -0600

    Revert "Se añadieron varias pages para el funcionamiento,librerias, implementacion de firebase, Auth y Firestore, modelos y algunos viewmodels."
    This reverts commit f793abbb48b7e79fbf0402741de25597ce7976da.

--- app/src/main/java/com/example/proyectodieta/data/models/MealReference.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyectodieta.data.models
-
-data class MealReference(
-    val mealId: String = "",
-    val mealName: String = "",
-    val dayOfWeek: String = "",
-    val servings: Int = 1
-)
